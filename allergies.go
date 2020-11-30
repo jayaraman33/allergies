e7df71fcf675d49b60ec4f150acd139840823742
jayaraman33 <70377145+jayaraman33@users.noreply.github.com>
2020-11-30 13:38:46 +0530

    first commit

---
+++ allergies.go
@@ -0,0 +1,37 @@
+package allergies
+
+var allergies = map[uint]string{
+	1:   "eggs",
+	2:   "peanuts",
+	4:   "shellfish",
+	8:   "strawberries",
+	16:  "tomatoes",
+	32:  "chocolate",
+	64:  "pollen",
+	128: "cats",
+}
+
+var keys = [8]uint{1, 2, 4, 8, 16, 32, 64, 128}
+
+func Allergies(score uint) []string {
+	result := make([]string, 0)
+
+	for _, key := range keys {
+		if key&score > 0 {
+			result = append(result, allergies[key])
+		}
+	}
+
+	return result
+}
+
+func AllergicTo(score uint, substance string) bool {
+
+	for _, key := range keys {
+		if key&score > 0 && allergies[key] == substance {
+			return true
+		}
+	}
+
+	return false
+}
